1cdb37036e8bc21983645ba33a8bfe87ee610c05
Ali Ansari <maliansari.coder@gmail.com>
2026-03-29 19:00:57 +0500

    added Cheep model/controller and changed home view to show actual cheeps from database

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,11 +23,27 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function cheeps(): HasMany
+    {
+        return $this->hasMany(Cheep::class);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,20 @@
         Home
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        <div class="card bg-base-100 shadow mt-8">
-            <div class="card-body">
-                <div>
-                    <h1 class="text-3xl font-bold">Welcome to Cheeper!</h1>
-                    <p class="mt-4 text-base-content/60">Cheeper is a simple microblogging platform where you can share
-                        your thoughts in short messages called "cheeps".</p>
+        @forelse ($cheeps as $cheep)
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold">{{ $cheep->user?->name ?? 'Anonymous' }}
+                        </div>
+                        <div class="mt-1">{{ $cheep->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $cheep->created_at->diffForHumans() }}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        @empty
+            <p class="text-gray-500">No cheeps yet. Be the first to cheep!</p>
+        @endforelse
     </div>
 </x-layout>
